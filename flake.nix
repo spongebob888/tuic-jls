a23d6df17496aa83749ab76d5d4770a47a2aa8de
spongebob888 <121923937+vincentliu77@users.noreply.github.com>
2025-04-05 12:16:24 +0800

    Adding flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        libraries = with pkgs;[
+          glib
+          openssl_3
+          sqlite
+          libclang
+        ];
+
+        packages = with pkgs; [
+          curlHTTP3
+          python
+          wget
+          sqlite
+          pkg-config
+          openssl_3
+          glib
+          cmake
+          protobuf
+          protoc-gen-rust
+		      clang
+          samply
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+
+          shellHook =
+            ''
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+              export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+            '';
+        };
+      });
+}
